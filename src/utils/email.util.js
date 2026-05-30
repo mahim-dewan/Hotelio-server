e4bb8993cf90f5cca5e2a026359bd57f095673d1
Mahim Dewan <mahimdewan79@gmail.com>
2026-05-30 18:46:55 +0600

    refactor(email): replace Resend with Brevo for email delivery
    - Removed Resend transporter dependency
    - Integrated Brevo for sending transactional emails
    - Improved email delivery compatibility without custom domain requirement

--- src/utils/email.util.js
+++ src/utils/email.util.js
@@ -1,15 +1,26 @@
 // src/utils/email.util.js
 
-const resend = require("../config/resend");
+const { response } = require("express");
 const ApiError = require("./apiError");
 
 const sendOtp = async ({ to, subject, template }) => {
+  const apiKey = process.env.BREVO_API_KEY;
+  const data = {
+    sender: { name: "Hotelio", email: "mahimdewan79@gmail.com" },
+    to: [{ email: to }],
+    subject: subject,
+    htmlContent: template,
+  };
+
   try {
-    await resend.emails.send({
-      from: `Hotelio <no-reply@resend.dev>`,
-      to: to,
-      subject,
-      html: template,
+    await fetch(`https://api.brevo.com/v3/smtp/email`, {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        accept: "application/json",
+        "api-key": apiKey,
+        "content-type": "application/json",
+      },
     });
   } catch (err) {
     console.error(`Email delivery failed to ${to}:`, err.message);
